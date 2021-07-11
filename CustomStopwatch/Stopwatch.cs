727de745ff0cffa20d973fa105496a52de05f449
caoanhquan <caoanhquan_qn@yahoo.com>
2021-07-11 23:34:10 +0700

    fix naming convetion in private field

--- CustomStopwatch/Stopwatch.cs
+++ CustomStopwatch/Stopwatch.cs
@@ -5,19 +5,19 @@ namespace CustomStopwatch
     {
         private static DateTime _start;
         private static DateTime _stop;
-        private static DateTime initialTime;
-        private static bool isRunning;
+        private static DateTime _initialTime;
+        private static bool _isRunning;
         public static bool GetIsRunning()
         {
-            return isRunning;
+            return _isRunning;
         }
         public static void Start()
         {
-            isRunning = true;
+            _isRunning = true;
             _start = DateTime.Now;
-            if (initialTime.Equals(default(DateTime)))
+            if (_initialTime.Equals(default(DateTime)))
             {
-                initialTime = _start;
+                _initialTime = _start;
             }
 
             Console.WriteLine("Start. Now is: " + _start);
@@ -26,13 +26,13 @@ namespace CustomStopwatch
         }
         public static void Stop()
         {
-            isRunning = false;
+            _isRunning = false;
             _stop = DateTime.Now;
             Console.WriteLine("Stop. Now is: " + _stop);
         }
         public static string GetDuration()
         {
-            isRunning = false;
+            _isRunning = false;
             var duration = _stop - _start;
             string elapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", duration.Hours, duration.Minutes, duration.Seconds,
                duration.Milliseconds);
@@ -40,16 +40,16 @@ namespace CustomStopwatch
         }
         public static string GetTotalDuration()
         {
-            isRunning = false;
-            var totalDuration = _stop - initialTime;
+            _isRunning = false;
+            var totalDuration = _stop - _initialTime;
             string totalElapsedTime = string.Format("{0:00}:{1:00}:{2:00}.{3:00}", totalDuration.Hours, totalDuration.Minutes, totalDuration.Seconds,
                totalDuration.Milliseconds);
             return totalElapsedTime;
         }
         public static void Reset()
         {
-            isRunning = false;
-            initialTime = default(DateTime);
+            _isRunning = false;
+            _initialTime = default(DateTime);
             _start = default(DateTime);
             _stop = default(DateTime);
         }
